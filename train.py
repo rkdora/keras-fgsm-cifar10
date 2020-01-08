8a6ba88d52152b07cf5ca1272cdfaf1b44502c29
ryuto <rryuto5210@gmail.com>
2020-01-08 14:33:51 +0900

    First commit

---
+++ train.py
@@ -0,0 +1,81 @@
+from keras.datasets import cifar10
+from keras.utils.np_utils import to_categorical
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten
+from keras.optimizers import Adam
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+batch_size = 10
+epochs = 30
+num_classes = 10
+
+# データの読み込み
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# 正規化
+x_train = x_train.astype(np.float32) / 255.0
+x_test = x_test.astype(np.float32) / 255.0
+
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(32,32,3)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+
+print(model.summary())
+
+hist = model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+
+model_json_str = model.to_json()
+open('cifar10_30_model.json', 'w').write(model_json_str)
+model.save_weights('cifar10_30_weights.h5')
+print('model saved')
+
+score = model.evaluate(x_test, y_test,verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# 正解率の推移をプロット
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.title('Accuracy')
+plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+plt.savefig("cifar10_30_acc.png")
+
+plt.cla()
+
+# ロスの推移をプロット
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('Loss')
+plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+plt.savefig("cifar10_30_loss.png")
+
+
